252313a8910e4def7b8011f81dc0329d7ec11099
diazric <eric.diaz13@yahoo.com>
2019-05-09 18:56:55 -0700

    Added Drop Station, changed some HTML

---
+++ drop_station.py
@@ -0,0 +1,28 @@
+import requests
+
+
+class Station:
+
+    def __init__(self, station):
+        self.station = station
+        self.url = "http://127.0.0.1/drop_station"
+
+    def make_the_drop(self, name):
+        if name[0] == "$" and name[-1] == "$":
+            name = name.strip("$")
+        else:
+            return
+
+        post_req = {
+            "station": self.station,
+            "picker": name
+        }
+        r = requests.post(url=self.url, data=post_req)
+        if r.status_code >= 300:
+            print(r.status_code)
+            print(self.url)
+            print(post_req)
+
+if __name__ == "__main__":
+    test_station = Station("test")
+    test_station.make_the_drop("$EricDiaz$")
