02a5f676fd164451b3b3488c0e2c08ff9f003326
diazric <eric.diaz13@yahoo.com>
2019-06-18 19:53:11 -0700

    search function utility library

---
+++ search_funcs.py
@@ -0,0 +1,26 @@
+import modals
+# Search queries for easy lookups
+
+
+def search_by_order(order_num, session):
+
+    entries = session.query(modals.Dematic).filter(modals.Dematic.sales_id == order_num).all()
+    return entries
+
+
+def search_by_batch(batch, session):
+
+    entries = session.query(modals.Dematic).filter(modals.Dematic.work_id == batch).all()
+    return entries
+
+
+def search_by_route(route, session):
+
+    entries = session.query(modals.Dematic).filter(modals.Dematic.route == route).all()
+    return entries
+
+
+def search_by_carton_id(carton, session):
+
+    entries = session.query(modals.Dematic).filter(modals.Dematic.suborder_id == carton).all()
+    return entries
